39f0da430bccd0c7a9b49f210fa963df135bffe9
VictorFabiano-Dev <victorfchaves@gmail.com>
2025-07-06 16:00:01 -0300

    Criando um projeto real com IA: Primeira experiência prática

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+
+function cliqueiNoBotao(){
+    let textoInput = document.querySelector(".input-animacao").value
+    let button = document.querySelector(".botao-magica")
+    button.disabled = true
+
+
+    console.log (textoInput)
+}
