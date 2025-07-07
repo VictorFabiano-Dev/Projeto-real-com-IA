ac737da0bf76c5c05c280dd1dc3913132ab35f89
VictorFabiano-Dev <victorfchaves@gmail.com>
2025-07-07 00:24:19 -0300

    implementando a ia

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 
 
+let webhook = "https://fabianovictor.app.n8n.cloud/webhook-test/animacao-css"
 
 
-function cliqueiNoBotao(){
+async function cliqueiNoBotao() {
     let textoInput = document.querySelector(".input-animacao").value
+    let codigo = document.querySelector(".area-codigo")
+    let areaResultado = document.querySelector(".area-resultado")
     let button = document.querySelector(".botao-magica")
     button.disabled = true
 
+    let resposta = await fetch(webhook, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ pergunta: textoInput })
+    })
 
-    console.log (textoInput)
+    let resultado = await resposta.json()
+
+    let info = JSON.parse(resultado.resposta)
+
+    console.log(info)
+
+    codigo.innerHTML = info.code
+
+    areaResultado.innerHTML = info.preview
+
+    document.head.insertAdjacentHTML('beforeend', "<style>" + info.style + "</style>")
 }
